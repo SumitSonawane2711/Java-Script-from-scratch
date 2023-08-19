008e4cb7e77c0e66459206696edc71f74a508771
SumitSonawane2711 <142674662+SumitSonawane2711@users.noreply.github.com>
2023-08-19 15:53:44 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+//used mdm and dc39 sites for js documentation for theory
+
+"use strict"; // treat all js code as newer version
+
+// alert (3+3) // we are using nodejs, not browserc
+
+/* premitive data types :
+  1.number => 2 to power 53
+  2.bigint
+  3.string => ""
+  4.boolean => true/false
+  5.null => this is the standalone value
+  6.undefined => 
+  7.symbol => to find the uniqueness
+
+*/
+
+ let age = 22
+ let name ="sumit"
+ let isLoggedIn = false
+ let state=null;
+ let city;
+
+//object 
+
+console.log(typeof age)
+console.log(typeof undefined);
+//if run the typeof of null then it gives object
+console.log(typeof null);
