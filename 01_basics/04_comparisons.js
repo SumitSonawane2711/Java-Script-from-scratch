57445cb2bc73041071fc9edfb898fe0a54807d4b
SumitSonawane2711 <142674662+SumitSonawane2711@users.noreply.github.com>
2023-08-23 18:12:26 +0000

    Strings nad comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,15 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+//Strict check
+console.log("2" === 2)   //strick check basically used trippel equalsto, it does not change the data type
+
+
+
---
+++ 01_basics/05_String.js
@@ -0,0 +1,21 @@
+const name = 'sumit'
+const myname=new String('sumit')
+const count=50
+
+// console.log(name + count + "value");     //Old way
+console.log(`hello my name is ${name} and my no is ${count}`)      //new way i.e String interpollation
+
+// console.log(myname[0])
+// console.log(myname.__proto__)
+
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.charAt(3));
+
+const newname='dnyaneshwar'
+
+const newString = newname.substring(0,4)
+console.log(newString);
+
+const anotherString=newname.slice(-8,8)
+console.log(anotherString)
