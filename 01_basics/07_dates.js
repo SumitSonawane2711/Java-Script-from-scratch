832e1bafb9a11f454dc6ff0415240419527b5dde
SumitSonawane2711 <142674662+SumitSonawane2711@users.noreply.github.com>
2023-09-10 08:29:05 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,13 @@
+
+let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleString());
+// console.log(typeof mydate);
+
+let myCreatedDate = new Date("03-01-2023")
+console.log(myCreatedDate.toLocaleDateString());
+
+console.log(Date.now());
