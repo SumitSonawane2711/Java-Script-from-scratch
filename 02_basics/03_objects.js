e53d71cd1a20300abafa1fe0defcf1c63f513610
SumitSonawane2711 <142674662+SumitSonawane2711@users.noreply.github.com>
2023-09-10 18:04:22 +0000

    objects basics

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,13 @@
+//singleton
+
+//object literals
+
+const Jsuser ={
+    name :"sumit",
+    age: 20,
+    location: "Nashik",
+    isLoggedIn: false
+}
+
+console.log(Jsuser.name);
+console.log(Jsuser["name"]);
