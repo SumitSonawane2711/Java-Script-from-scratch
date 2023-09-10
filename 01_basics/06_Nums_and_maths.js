90152a78caa059e069d9e39deab17db8c2f4e630
SumitSonawane2711 <142674662+SumitSonawane2711@users.noreply.github.com>
2023-09-10 08:11:16 +0000

    math and num

---
+++ 01_basics/06_Nums_and_maths.js
@@ -0,0 +1,37 @@
+const score =400
+
+const balance =new Number(100)
+// console.log(score)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1))
+
+const otherNumber = 123.3456
+
+//console.log(otherNumber.toPrecision(2));
+
+const hundreds = 100000
+//console.log(hundreds.toLocaleString('en-IN'))
+
+//++++++++++++++ Maths +++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4))
+// console.log(Math.round(5.7));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(5.7));
+// console.log(Math.pow(5));
+// console.log(Math.min(5,6,9,2,4));
+// console.log(Math.max(5,6,9,2,4));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max =20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
