ba7785549cdcb8c5e3983dba9d54c7e8b95b2f38
SumitSonawane2711 <142674662+SumitSonawane2711@users.noreply.github.com>
2023-10-03 09:44:29 +0000

    commited promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -56,18 +56,49 @@ promiseFour.then((user)=>{
 
 
 
-const promiseFive = new Promise(function(resolve,reject){
+const PromiseFive = new Promise(function(resolve,reject){
     setTimeout(function(){
         let error = true
         if(!error){
             resolve({username:'roshan', password:'123'})
         }
         else{
-            reject({'ERROR: JS went wrong'})
+            reject('ERROR: JS went wrong')
         }
     },1000)
 });
 
-async function consumePromiseFive({
-    const response = await promises
-}) 
+async function consumePromiseFive(){
+   try{
+    const response = await PromiseFive
+    console.log(response);
+   }catch(error){
+    console.log(error);
+   }
+}
+
+consumePromiseFive()
+
+
+// async function getAllUsers(){
+//     try{
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data);
+//     }catch (error){
+//         console.log("E: ",error)
+//     }
+// }
+
+// getAllUsers()
+
+//*******we can directly write above commented code in fetch
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
