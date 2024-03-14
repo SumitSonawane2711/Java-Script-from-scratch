2faf0dc612361491b919215b9eed94628d8ff14c
SumitSonawane2711 <142674662+SumitSonawane2711@users.noreply.github.com>
2024-03-14 17:29:50 +0000

    call in js

--- 10_classes_and_oop/call.js
+++ 10_classes_and_oop/call.js
@@ -5,7 +5,7 @@ function SetUsername(username){
 
 function createUser(username, email, password){
     // SetUsername(username) 
-    // SetUsername(username)
+    // SetUsername.call(username)
     SetUsername.call(this,username)
 
     this.email = email
